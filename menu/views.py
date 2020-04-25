d6f422a5dbc72d667e6240552e59b3b84c9bcc7b
olyawh <42892491+olyawh@users.noreply.github.com>
2020-04-25 14:40:31 +0300

    Updated create_new_menu view

--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
 from django.utils import timezone
-from operator import attrgetter
 from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
 from .models import *
 from .forms import *
+
 
 def menu_list(request):
     '''View that shows a list of all menus filtered by expiration date'''
@@ -19,27 +19,35 @@ def menu_list(request):
     #menus = sorted(menus, key=attrgetter('expiration_date'))
     return render(request, 'menu/list_all_current_menus.html', {'menus': menus})
 
+
 def menu_detail(request, pk):
     '''View that shows a menu's details'''
     menu = get_object_or_404(Menu, pk=pk)
     return render(request, 'menu/menu_detail.html', {'menu': menu})
+
 
 def item_detail(request, pk):
     '''View that displays a menu's item details'''
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'menu/detail_item.html', {'item': item})
 
+
 def create_new_menu(request):
+    '''A view to create and save new menu'''
     if request.method == "POST":
         form = MenuForm(request.POST)
+
+        '''Checking if the form is valid'''
         if form.is_valid():
             menu = form.save(commit=False)
             menu.created_date = timezone.now()
             menu.save()
+            menu.save_m2m()
             return redirect('menu_detail', pk=menu.pk)
     else:
         form = MenuForm()
     return render(request, 'menu/menu_edit.html', {'form': form})
+
 
 def edit_menu(request, pk):
     menu = get_object_or_404(Menu, pk=pk)
